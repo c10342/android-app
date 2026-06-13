bae544221dc5ee7fec85604e48cd3eb07825fd11
c10342 <1376023740@qq.com>
2026-06-13 22:38:36 +0800

    update

--- app/src/main/java/com/example/myapp/SplashActivity.java
+++ app/src/main/java/com/example/myapp/SplashActivity.java
@@ -1,6 +1,9 @@
 package com.example.myapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.myapp.activity.LoginActivity;
+import com.example.myapp.activity.RegisterActivity;
+
 public class SplashActivity extends AppCompatActivity {
+
+    private Button btnLogin;
+    private  Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +28,24 @@ public class SplashActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        btnLogin = findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnRegister = findViewById(R.id.btn_register);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SplashActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
